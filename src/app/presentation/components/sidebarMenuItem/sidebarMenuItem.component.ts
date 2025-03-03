fc1cd34fa9214b1a6caaec79f6dd316fda1f3e8b
Doplax <doplax@gmail.com>
2025-03-03 20:10:40 +0100

    feat: install Angular Markdown

--- src/app/presentation/components/sidebarMenuItem/sidebarMenuItem.component.ts
+++ src/app/presentation/components/sidebarMenuItem/sidebarMenuItem.component.ts
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
     <a
       [routerLink]="path"
       routerLinkActive="bg-gray-800"
-      class="flex justify-center items-center hover:bg-gray-800 rounded-md p-2 transition-colors"
+      class="flex justify-center items-center hover:bg-gray-800 rounded-md p-2 transition-colors cursor-pointer"
     >
       <i class="{{ icon }} text-2xl mr-4 text-indigo-400"></i>
       <div class="flex flex-col flex-grow">
@@ -26,5 +26,6 @@ export class SidebarMenuItemComponent implements OnInit {
   @Input({ required: true }) path!: string;
 
   ngOnInit() {
+    console.log(this.path);
   }
 }
