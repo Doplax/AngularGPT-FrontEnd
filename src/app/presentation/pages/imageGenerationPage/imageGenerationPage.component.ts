881e6b99cc6419d16c2495b7bba9eea6334a2996
Doplax <doplax@gmail.com>
2025-03-26 13:05:58 +0100

    feat: Continued application

--- src/app/presentation/pages/imageGenerationPage/imageGenerationPage.component.ts
+++ src/app/presentation/pages/imageGenerationPage/imageGenerationPage.component.ts
@@ -1,9 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent } from '@components/chat-bubbles/chatMessage/chatMessage.component';
+import { MyMessageComponent } from '@components/chat-bubbles/myMessage/myMessage.component';
+import { TextMessageBoxComponent } from '@components/text-boxes/textMessageBox/textMessageBox.component';
+import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.component';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 @Component({
   selector: 'app-image-generation-page',
-  imports: [],
-  templateUrl: './imageGenerationPage.component.html',
+  imports: [
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+  ],  templateUrl: './imageGenerationPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ImageGenerationPageComponent { }
+export default class ImageGenerationPageComponent {
+    public messages = signal<Message[]>([]);
+    public isLoading = signal<boolean>(false);
+    public openAiService = inject(OpenAiService);
+
+    handleMessage(prompt: string) {
+      console.log(prompt);
+    }
+
+ }
