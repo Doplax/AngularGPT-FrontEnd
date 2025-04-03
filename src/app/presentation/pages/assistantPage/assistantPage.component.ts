bd6bc24962f623e75e6bf1c20229cde300c90209
Doplax <doplax@gmail.com>
2025-04-03 16:42:14 +0200

    feat: Thread Creation

--- src/app/presentation/pages/assistantPage/assistantPage.component.ts
+++ src/app/presentation/pages/assistantPage/assistantPage.component.ts
@@ -1,9 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-assistant-page',
-  imports: [],
-  templateUrl: './assistantPage.component.html',
+  imports: [
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+  ],  templateUrl: './assistantPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AssistantPageComponent { }
+export default class AssistantPageComponent {
+    public messages = signal<Message[]>([]);
+    public isLoading = signal<boolean>(false);
+    public openAiService = inject(OpenAiService);
+
+    handleMessage(prompt: string) {
+      console.log(prompt);
+    }
+ }
